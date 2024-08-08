9c37e091d2286e05117971cb5cac94dcda811e04
David Umoru <umorudavido@gmail.com>
2024-08-08 17:21:41 +0100

    Replace placeholders  with user-provided answers

--- readme-generator/lib/generateReadme.js
+++ readme-generator/lib/generateReadme.js
@@ -1,21 +1,3 @@
-module.exports = (answers) => {
-  return `
-# ${answers.projectName}
-
-## Description
-${answers.description}
-
-## Installation
-\`\`\`
-${answers.installation}
-\`\`\`
-
-## Usage
-\`\`\`
-${answers.usage}
-\`\`\`
-
-## License
-This project is licensed under the ${answers.license} License.
-    `;
+module.exports = (templateContent, answers) => {
+  return templateContent.replace(/{{(\w+)}}/g, (_, key) => answers[key] || "");
 };
